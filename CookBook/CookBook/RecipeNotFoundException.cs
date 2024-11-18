b55f303e2e6b6cfc53955a2e257061414da9f946
Dzana <dkrnjic1@etf.unsa.ba>
2024-11-18 14:53:42 +0100

    deleted files

--- CookBook/CookBook/RecipeNotFoundException.cs
+++
@@ -1,7 +0,0 @@
-﻿public class RecipeNotFoundException : Exception
-{
-    public RecipeNotFoundException(string naziv)
-        : base($"Recept sa nazivom '{naziv}' nije pronađen.")
-    {
-    }
-}
